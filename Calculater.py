3a14fe9e51e750a5b71709a838072484dc10f4a9
Farzad Ebrahimy <120629523+Farzad-0x0@users.noreply.github.com>
2023-03-08 10:50:10 +0330

    Update Calculater.py

--- Calculater.py
+++ Calculater.py
@@ -1,7 +1,4 @@
 import discord
-import asyncio
-import random
-import datetime
 import colorama
 from discord.ext import commands
 from discord.ui import Button , View , Select
@@ -11,7 +8,7 @@ init(convert=True)
 
 
 
-token = "MTA4MTU5ODE2OTk0MDQ5MjM4MA.GV_nLL.z_AZEgpkeqkrnRmtIgeK2nWI4F_qio-Fz1vODA"
+token = "Your token"
 
 
 intents = discord.Intents.all()
@@ -19,16 +16,6 @@ bot = commands.Bot(command_prefix= "CL!",
                    intents= intents ,
                 status = discord.Status.online ,
                 activity = discord.Activity(type = discord.ActivityType.competing , name= "Numbers"))
-
-
-
-@bot.event
-async def on_connect():
-   print(Fore.CYAN+"-->" , Fore.LIGHTGREEN_EX+"Calculator bot connected to" , Fore.RED+"Discord API")
-   print(Fore.CYAN+"-->",Fore.LIGHTGREEN_EX+f"Calculator bot joined to" , Fore.RED+f"{len(bot.guilds)} server")
-   print("""""")
-
-
 
 
 @bot.event
@@ -43,76 +30,6 @@ async def on_ready():
    
 
 
-
-bot.remove_command('help')
-@bot.tree.command(name="help" , description= "Help command 🌲")
-async def help(interaction:discord.Interaction):
-
-
-   help_embed = discord.Embed(
-      title= "🔎  Welcome to my help desk !" , description= "What do you need help with?" , color= discord.Colour.dark_gray()
-   )
-   commands_embed = discord.Embed(
-      title= "List of commands :" , description= """
-      My prefix = **CL!**
-      Note : This bot supported SlashCommands <:slashsupported:1078221808030994532>""" , color= discord.Colour.dark_gray()
-   )
-   about_embed = discord.Embed(
-      title= "About me" , description= """Hi 👋🏻
-      I am a little calculator !
-      I can't do professional calculations, but I'm smart 😅
-      Thanks for using me 💋
-
-      > *Programmed with love* :anatomical_heart:
-      """ , color = discord.Colour.dark_gray()
-   )
-
-
-   selector = Select(placeholder= "🔒 Select the section you need"
-                  , options= [discord.SelectOption(label= "About" , description= "About me" , emoji= "<:emoji_qm:1074693739827712050>"),
-                              discord.SelectOption(label= "Commands" ,description= "List of commands" , emoji= "<:emoji_scb:1074693374835175534>")])
-   
-
-   async def callback(interaction:discord.Interaction):
-      if selector.values[0] == "About" :
-         await interaction.response.send_message(embed= about_embed , ephemeral= True)
-      if selector.values[0] == "Commands" :
-         await interaction.response.send_message(embed= commands_embed , ephemeral= True)
-
-
-   selector.callback = callback
-
-   view = View()
-   view.add_item(selector)
-   
-   await interaction.response.send_message(embed= help_embed , view= view , ephemeral= True)
-
-
-
-
-
-@bot.tree.command(
-      name="bug" , description= 'Report problem or bug to support !'
-)
-@app_commands.describe(your_report = "type :")
-async def bug(interaction:discord.Interaction , your_report:str):
-   support_channel = bot.get_channel(1082358728730361957)
-   content_embed = discord.Embed(
-      title= "New report 📄" , description= f"""
-      Content :
-      **{your_report}**
-      Time : {interaction.created_at.date()}
-      From : {interaction.user.mention}""" , colour= discord.Colour.green()
-   )
-   await support_channel.send(embed=content_embed)
-
-
-   send_embed = discord.Embed(
-      title= "✅ Report sent successfully!" , description= "Thanks for your feedback 👋" ,color=discord.Colour.random()
-   )
-   
-
-   await interaction.response.send_message(embed=send_embed)
 
 #_____________________________________________________________________________________________________
 #_____________________________________________________________________________________________________
